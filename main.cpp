6704fcf7922696d890404683c193b5f3a7f64d04
Itali1 <ipizarrovalent9398@sdsu.edu>
2025-02-02 16:21:41 -0800

    Completed homework assignment

--- main.cpp
+++ main.cpp
@@ -11,12 +11,18 @@ private:
 
 public:
     // Constructor
+    Robot(string n, string m, int b): name(n), model(m), batteryLife(b){}
 
 
     // Getter Methods
-
+    string getName(){return name;}
+    string getModel(){return model;}
+    int getBatteryLife(){return batteryLife;}
 
     // Setter Methods
+    void setName(string n){name = n;}
+    void setModel(string m){model = m;}
+    void setBatteryLife(int b){batteryLife = b;}
 
 
     // Display function
@@ -26,9 +32,14 @@ public:
 };
 
 // Step 2: Function to modify robot (pass by value)
-
+    void modifyingRobotbyValue(Robot r) {
+        cout<<"Modifying by value"<<endl;
+    }
 
 // Step 3: Function to modify robot (pass by reference)
+    void modifyingRobotbyReference(Robot& r) {
+        cout<<"Modifying by reference"<<endl;
+    }
 
 
 // Step 4: Template class for a Fleet that stores multiple robots
@@ -73,20 +84,19 @@ public:
 
 int main() {
     // Step 5: Create a Robot object
-
-
+    Robot myRobot("Robot1", "Version1", 89);
 
     // Step 6: Use pointers to access Robot object
-
-   // cout << "Updated Battery Life (using pointer): " << robotPtr->getBatteryLife() << "%\n";
-
+    Robot* robotPtr = &myRobot;
+    cout << "Updated Battery Life (using pointer): " << robotPtr->getBatteryLife() << "%\n";
+    robotPtr->displayRobot();
     // Step 7: Pass by value (no change outside function)
-
-   // cout << "After modifyRobotByValue, Battery Life: " << myRobot.getBatteryLife() << "%\n";
+    modifyingRobotbyValue(myRobot);
+    cout << "After modifyRobotByValue, Battery Life: " << myRobot.getBatteryLife() << "%\n";
 
     // Step 8: Pass by reference (changes persist)
-
-   // cout << "After modifyRobotByReference, Battery Life: " << myRobot.getBatteryLife() << "%\n";
+    modifyingRobotbyReference(myRobot);
+    cout << "After modifyRobotByReference, Battery Life: " << myRobot.getBatteryLife() << "%\n";
 
     // Step 9: Use the Fleet template class
     Fleet<string> myFleet(3);
